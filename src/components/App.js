4b907a4f83e3de1f4370e7f046354626915de4fb
nerldy <arnodhis@gmail.com>
2018-08-12 20:57:03 +0300

    [Feature #159719812] Add Sign Up Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import SignUp from "./Signup";
 
 
 class App extends Component {
@@ -6,6 +7,7 @@ class App extends Component {
         return (
             <div>
                 hello books
+                <SignUp/>
             </div>
         );
     }
