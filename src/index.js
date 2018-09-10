37d497f441edeb47c37f8c49c4172710f77c60db
nerldy <arnodhis@gmail.com>
2018-09-10 10:53:07 +0300

    Format doc

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import 'bulma/css/bulma.css'
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import "bulma/css/bulma.css";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+ReactDOM.render(<App />, document.getElementById("root"));
